d630ea09f15600f5f6cb1b05d7d697c8be2e962f
Mohit Saini <mohitms@google.com>
2022-07-10 19:37:06 +0100

    Second commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MainFunc from './common';
-import {setApiBaseUrl} from './library';
-
-// setApiBaseUrl("http://localhost:5504")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
