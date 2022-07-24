4a4aa9a8e3a122623723eb46d5739c868a75b935
Mohit Saini <mohitsaini1196@gmail.com>
2022-07-24 18:29:22 +0100

    Replace Link tag by 'a' tag in header

--- src/header.jsx
+++ src/header.jsx
@@ -1,24 +1,23 @@
 import './library.css';
-import {Link} from "react-router-dom"
 import React from 'react';
 
 export function Header() {
   return (
     <div className="hsplit" >
       <div>
-        <Link to="/example1">
+        <a href="#/example1">
           <div className="link_box" >Example1</div>
-        </Link>
+        </a>
       </div>
       <div>
-        <Link to="/example2">
+        <a href="#/example2">
           <div className="link_box" >Example2</div>
-        </Link>
+        </a>
       </div>
       <div>
-        <Link to="/example3">
+        <a href="#/example3">
           <div className="link_box" >Example3</div>
-        </Link>
+        </a>
       </div>
     </div>
   );
