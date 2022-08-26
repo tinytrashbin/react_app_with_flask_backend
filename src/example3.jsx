163aa2b9248bad9bf99c63353a5548b64805475e
Mohit Saini <mohitsaini1196@gmail.com>
2022-08-26 12:35:12 +0100

    Material icons

--- src/example3.jsx
+++ src/example3.jsx
@@ -1,13 +1,17 @@
 import {Header} from './header';
 import React from 'react';
+import Button from '@mui/material/Button';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 function Example3() {
   return (
     <div >
       <Header/>
+      <Button variant="contained">Hello World</Button>;
       <div>
-        Example3
+        Example3...
       </div>
+      Icon = <AcUnitIcon/>
     </div>
   );
 }
